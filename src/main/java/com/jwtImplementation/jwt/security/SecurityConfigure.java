58d6108e2a85c9b1f80c062d5804643552960dc2
NitinSP <nitin@srslive.in>
2020-09-25 19:33:35 +0530

    complete

--- src/main/java/com/jwtImplementation/jwt/security/SecurityConfigure.java
+++ src/main/java/com/jwtImplementation/jwt/security/SecurityConfigure.java
@@ -7,15 +7,21 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.jwtImplementation.jwt.filter.JwtRequestFilter;
 import com.jwtImplementation.jwt.service.MyUserDetailService;
 
 @EnableWebSecurity
 public class SecurityConfigure extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private MyUserDetailService myUserDetailService;
+	
+	@Autowired
+	JwtRequestFilter jwtRequestFilter;
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -39,7 +45,10 @@ public class SecurityConfigure extends WebSecurityConfigurerAdapter {
 		// TODO Auto-generated method stub
 		http.csrf().disable().authorizeRequests()
 		.antMatchers("/authenticate").permitAll()
-		.anyRequest().authenticated();
+		.anyRequest().authenticated()
+		.and().sessionManagement()
+		.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 	}
 
 }
